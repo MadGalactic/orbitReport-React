27066daa8c3e3064bd67c1fa43619f2e34e073b9
Madeline Lora <MVDLNE@Madelines-MacBook.local>
2023-08-11 13:39:33 -0400

    finished

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,16 +3,33 @@ const Table = ({ sat }) => {
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
+          {sat.map((data, index) => (
+            <tr key={sat.id}>
+            <td>{data.name}</td>
+            <td>{data.type}</td>
+            <td>{data.launchDate}</td>
+            <td>{data.operational ? 'Active' : 'Inactive'}</td>
+            </tr>
+          ))}
+        
         </tbody>
       </table>
   );
 };
 
 export default Table;
+
+// {displaySats.map((sat, id) => {
+//   return (
+//     <button onClick={() => filterByType(sat)} key={id}>
+//       {sat} Orbit
+//     </button>
+//   );
+// })}
